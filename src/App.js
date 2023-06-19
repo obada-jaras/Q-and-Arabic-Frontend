3cebc96b5e39854839096deb8d07ffb613b7d709
OBADA TAHAYNA <obada.jaras@yahoo.com>
2023-06-19 03:37:56 +0300

    initial project setup: added file structure, base components, pages, routing, and global styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import HistoryPage from './pages/HistoryPage/HistoryPage';
+import BookmarkPage from './pages/BookmarkPage/BookmarkPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/home" component={HomePage} />
+        <Route path="/history" component={HistoryPage} />
+        <Route path="/bookmark" component={BookmarkPage} />
+        <Route path="/" exact component={LoginPage} />
+      </Routes>
+    </Router>
   );
 }
 
